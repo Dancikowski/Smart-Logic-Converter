c5ea905ff5e1e6178ee574c8c5b99b42596cf935
Dancikowski <32976044+Dancikowski@users.noreply.github.com>
2018-06-30 10:30:24 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,7 @@ require(['logic'], function (logic) {
 
 
 
+
     function regexIndexOf(text, re, i) {
         var indexInSuffix = text.slice(i).search(re);
         return indexInSuffix < 0 ? indexInSuffix : indexInSuffix + i;
@@ -63,58 +64,92 @@ require(['logic'], function (logic) {
         return bool;
 
     }
-
+    const arrayOfSymbolKeys = [80, 81, 82, 192, 187, 54, 86, 190, 61, 57, 48];
     const text = document.querySelector('.type');
     let stack = [];
+    const container = document.querySelector('.mobileContainer');
+    console.log(window.innerWidth);
+    if (window.innerWidth > 500) {
+        document.addEventListener('keydown', function (el) {
+            text.style.color = '3b3b3b';
+            console.log(el.keyCode);
+            if (el.keyCode != 13) {
+                if (el.keyCode != 16) {
+                    if (arrayOfSymbolKeys.indexOf(el.keyCode) != -1) {
 
-    document.addEventListener('keydown', function (el) {
-        text.style.color = '3b3b3b';
-        console.log(el.keyCode);
-        if (el.keyCode != 13) {
-            if (el.keyCode != 16) {
-                if (el.keyCode != 8) {
-                    let a = el.keyCode == 190 ? '⇒' : el.keyCode == 61 || el.keyCode == 187 ? '⇔' : el.keyCode == 192 ? '~' : el.keyCode == 54 ? '∧' : el.keyCode == 86 ? '∨' : el.key;
-                    stack.push(a);
+                        let a = el.keyCode == 190 ? '⇒' : el.keyCode == 61 || el.keyCode == 187 ? '⇔' : el.keyCode == 192 ? '~' : el.keyCode == 54 ? '∧' : el.keyCode == 86 ? '∨' : el.key;
+                        stack.push(a);
 
-                    text.textContent = stack.join('');
-                    console.log(stack);
-                } else {
-                    text.style.color = '';
-                    el.preventDefault();
-                    stack.pop();
-                    text.textContent = stack.join('');
+                        text.textContent = stack.join('');
+                        console.log(stack);
+                    } else if (el.keyCode == 8) {
+                        text.style.color = '';
+                        el.preventDefault();
+                        stack.pop();
+                        text.textContent = stack.join('');
+                    }
                 }
+            } else text.textContent = stack.join('');
+        })
+    }
+    if (window.innerWidth > 500) {
+        document.querySelector('.go').addEventListener('click', function () {
+
+            let formula = text.textContent.split('').map(function (el) {
+
+                return el == '⇒' ? '>' : el == '⇔' ? '=' : el;
+            }).join('');
+
+
+            if (!checkFormula(formula) || !initialCheck(formula)) {
+                stack = [];
+                text.style.color = '#e05454';
+                text.textContent = 'Invalid Syntax!';
+            } else {
+
+                let result = logic.convertFormula(formula).split('').map(function (el) {
+                    return el == '>' ? '⇒' : el == '=' ? '⇔' : el;
+                }).join('');
+
+                result = removeDuplicateNegations(result);
+                stack = result.split('');
+                text.textContent = result;
             }
-        } else text.textContent = stack.join('');
-    })
-
-    document.querySelector('.go').addEventListener('click', function () {
-        let formula = text.textContent.split('').map(function (el) {
-
-            return el == '⇒' ? '>' : el == '⇔' ? '=' : el;
-        }).join('');
+        })
+    } else {
 
 
-        if (!checkFormula(formula) || !initialCheck(formula)) {
-            stack = [];
-            text.style.color = '#e05454';
-            text.textContent = 'Invalid Syntax!';
-        } else {
-
-            let result = logic.convertFormula(formula).split('').map(function (el) {
+        container.querySelector('.go').addEventListener('click', function () {
+            let formula = container.querySelector('#formula').value;
+            let yourFormula = formula.split('').map(function (el) {
                 return el == '>' ? '⇒' : el == '=' ? '⇔' : el;
             }).join('');
 
-            result = removeDuplicateNegations(result);
-            stack = result.split('');
-            text.textContent = result;
-        }
+            container.querySelector('.yourFormula pre').textContent = yourFormula;
+            let result = logic.convertFormula(formula);
+            container.querySelector('.result pre').textContent = result;
+            container.querySelector('.result').style.display = "block";
+            container.querySelector('.yourFormula').style.display = "block";
 
+        })
+
+
+
+
+    }
+
+    container.querySelector('.clear').addEventListener('click', function () {
+        container.querySelector('#formula').value = "";
+        stack = [];
+        text.textContent = "";
     })
+
 
     document.querySelector('.clear').addEventListener('click', function () {
 
         stack = [];
         text.textContent = "";
+
     })
+
 })
