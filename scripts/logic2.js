4c1731e6ab682ea7fbdba37210f554008a0ff6bd
Dancikowski <32976044+Dancikowski@users.noreply.github.com>
2018-07-10 15:30:16 +0200

    Update logic2.js

--- scripts/logic2.js
+++ scripts/logic2.js
@@ -228,7 +228,7 @@ define('logic', ['script'], function () {
 
                         var newPartFormula = "";
                         if (join == '>') {
-                            newPartFormula = '~' + a + 'V' + b;
+                            newPartFormula = '~' + a + '∨' + b;
                           
                             helper = helper.split('');
                             let firstInd = firstIndexBackwards(form, a, i)
